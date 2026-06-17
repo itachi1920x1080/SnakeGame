33723de8d1114ad3b448a1bdc549e7cfe9fa697b
itachi1920x1080 <itachi1920x1080@gmail.com>
2026-06-17 15:21:45 +0700

    Draw snake head

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ pygame.display.set_caption('Snake Game')
 clock = pygame.time.Clock()
 
 # Player
-player_x = 375
-player_y = 275
-player_size = 50
+snake_x = 375
+snake_y = 275
+snake_size = 50
 speed = 5
 
 
@@ -28,17 +28,17 @@ while runing:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        player_x-=speed
+        snake_x-=speed
     elif keys[pygame.K_RIGHT]:
-        player_x+=speed
+        snake_x+=speed
     elif keys[pygame.K_UP]:
-        player_y-=speed
+        snake_y-=speed
     elif keys[pygame.K_DOWN]:
-        player_y+=speed
+        snake_y+=speed  
 
     # draw
     screen.fill((0,0,0))
-    pygame.draw.rect(screen,(0,255,0), pygame.Rect(player_x, player_y, player_size, player_size))
+    pygame.draw.rect(screen,(0,255,0), pygame.Rect(snake_x, snake_y, snake_size, snake_size))
     pygame.display.update()
     clock.tick(60)
 
