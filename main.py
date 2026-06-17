82850f00814b982487eb643d5bd2cb679dbfb62f
itachi1920x1080 <itachi1920x1080@gmail.com>
2026-06-17 15:15:00 +0700

    Add player movement

--- main.py
+++ main.py
@@ -10,13 +10,35 @@ HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Snake Game')
 clock = pygame.time.Clock()
+
+# Player
+player_x = 375
+player_y = 275
+player_size = 50
+speed = 5
+
+
 runing = True
 
 while runing:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runing = False
+    # Keyvord input
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player_x-=speed
+    elif keys[pygame.K_RIGHT]:
+        player_x+=speed
+    elif keys[pygame.K_UP]:
+        player_y-=speed
+    elif keys[pygame.K_DOWN]:
+        player_y+=speed
+
+    # draw
     screen.fill((0,0,0))
+    pygame.draw.rect(screen,(0,255,0), pygame.Rect(player_x, player_y, player_size, player_size))
     pygame.display.update()
     clock.tick(60)
 
